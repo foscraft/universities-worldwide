a0bef6a5810653615056264900a29916e5d3ec7c
foscraft <anyaribari@gmail.com>
2023-04-19 08:58:50 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+World-universities-csv
+======================
+This is a forked copy of two CSV files with universities in Africa and around the world.
+
+I have modified this to only include the University name and the .edu domain name that is associated with the university.  
+
+This is useful for identifying a college based on an email address.
+
+
---
+++ import.py
@@ -0,0 +1,48 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+import asyncio
+from country_data import countries
+
+count = 0
+lastPage = None
+
+async def readPage(page, write, cb):
+    global count, lastPage
+    response = requests.get(page)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    if firstItem := soup.select_one('ol li a'):
+        currentPage = firstItem.text
+        if currentPage == lastPage:
+            return cb()
+        lastPage = currentPage
+    for el in soup.select('ol li a'):
+        write(el.text, el['href'])
+        count += 1
+    cb()
+
+output = csv.writer(open('world-universities.csv', 'w', encoding='utf8'))
+
+async def loadList(dom, country, cb):
+    global count
+    total = 0
+    start = 1
+    print(f"[{country}] ", end="")
+    while True:
+        page = f"http://univ.cc/search.php?dom={dom}&key=&start={start}"
+        await readPage(page, lambda name, url: output.writerow([country, name, url]), lambda: None)
+        start += 50
+        total += count
+        print('.', end="")
+        if count < 50:
+            break
+    print(total)
+    cb()
+
+countriesCodes = countries.keys()
+for country in countriesCodes:
+    if len(country) != 2:
+        continue
+    dom = "edu" if country == "US" else country
+    asyncio.run(loadList(dom.lower(), country, lambda: None))
+
